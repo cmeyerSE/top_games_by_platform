29cb65ed1fefa531c261a8014f5de4df85bef192
Cory <cameyer182@gmail.com>
2022-06-24 21:11:07 -0400

    added a get_platforms and get_user_platforms so it lists all the platforms they can choose to find games.

--- lib/top_games_by_platform/cli.rb
+++ lib/top_games_by_platform/cli.rb
@@ -1,6 +1,21 @@
 class TopGamesByPlatform::CLI
     def call
         puts "Welcome! Let's find your next game!"
-        binding.pry
+        get_platforms
+        get_user_platform
+        #get_games_for(platform)
+        #list_games
+    end
+
+    def get_platforms
+        #to be scraped instead
+        @platforms = ['Xbox X/S', 'PS5', 'PC', 'Switch']
+    end
+
+    def get_user_platform
+        # list platforms
+        @platforms.each.with_index(1) do |platform, index|
+            puts "#{index}. #{platform}"
+        end
     end
 end
