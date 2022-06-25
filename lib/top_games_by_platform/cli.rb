dda645d61edc19a6afe86f25d2c64603b081f2b6
Cory <cameyer182@gmail.com>
2022-06-24 21:55:00 -0400

    added a show games method, which will eventually add a list of the top games for the chosen platform once its finalized

--- lib/top_games_by_platform/cli.rb
+++ lib/top_games_by_platform/cli.rb
@@ -14,21 +14,29 @@ class TopGamesByPlatform::CLI
     end
 
     def list_platforms
-        puts "Choose your platform number:"
+        puts "\nChoose your platform number:\n"
         # list platforms
         @platforms.each.with_index(1) do |platform, index|
-            puts "#{index}. #{platform}"
+            puts "\n#{index}. #{platform}\n"
         end
     end
 
     def get_user_platform
-        chosen_platform = gets.strip
-        binding.pry
-       # if valid_input(chosen_platform, @platforms)
-       # end
+        chosen_platform = gets.strip.to_i
+        show_games_for(chosen_platform) if valid_input(chosen_platform, @platforms)
     end
 
     def valid_input(input, data)
         input.to_i <= data.length && input.to_i > 0
+    end
+
+    def show_games_for(chosen_platform)
+        platform = @platforms[chosen_platform - 1]
+        puts "Top games for #{platform}:"
+        ## to implement
+        #TopGamesByPlatform::Game.all.each.with_index(1) do | game |
+        #    puts game.name
+        #end
+        #get_games
     end
 end
