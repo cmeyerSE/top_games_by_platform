90d1c67e71f9f12cdf6202d829c017dd286a52ee
Cory <cameyer182@gmail.com>
2022-06-24 21:38:41 -0400

    added a validation check to make sure the data input in app works

--- lib/top_games_by_platform/cli.rb
+++ lib/top_games_by_platform/cli.rb
@@ -1,7 +1,8 @@
 class TopGamesByPlatform::CLI
     def call
-        puts "Welcome! Let's find your next game!"
+        puts "\nWelcome! Let's find your next game!\n"
         get_platforms
+        list_platforms
         get_user_platform
         #get_games_for(platform)
         #list_games
@@ -12,10 +13,22 @@ class TopGamesByPlatform::CLI
         @platforms = ['Xbox X/S', 'PS5', 'PC', 'Switch']
     end
 
-    def get_user_platform
+    def list_platforms
+        puts "Choose your platform number:"
         # list platforms
         @platforms.each.with_index(1) do |platform, index|
             puts "#{index}. #{platform}"
         end
+    end
+
+    def get_user_platform
+        chosen_platform = gets.strip
+        binding.pry
+       # if valid_input(chosen_platform, @platforms)
+       # end
+    end
+
+    def valid_input(input, data)
+        input.to_i <= data.length && input.to_i > 0
     end
 end
